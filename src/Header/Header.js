4a753c33865e7967a8d01a049c3105527f243dc2
Chad Drake <chaddrake1313@yahoo.com>
2020-10-08 10:06:57 -0500

    added functionality to Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import tokenService from "../services/token-service";
 
 export default class Nav extends Component {
   render() {
     return (
       <section>
-        <button type="button">Home</button>
-        <button type="button">Add New Metric</button>
-        <button type="button">Logout</button>
+        <h1> FitNotes</h1>
+        <Link to="/home">
+          <button type="button">Home</button>
+        </Link>
+        <Link to="metrics">
+          <button type="button">Add New Metric</button>
+        </Link>
+        <button onClick={tokenService.clearAuthToken} type="button">
+          Logout
+        </button>
       </section>
     );
   }
