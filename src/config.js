b7a4f82a9fe28b39bc7de9044be0808a541a78d5
Chad Drake <chaddrake1313@yahoo.com>
2020-10-16 13:21:57 -0500

    updating config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://salty-basin-04383.herokuapp.com',
+  API_ENDPOINT: 'https://hidden-falls-68641.herokuapp.com',
   TOKEN_KEY: 'e47b9dfc-08c9-11eb-adc1-0242ac120002',
 };
