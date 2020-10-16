02cfc3d8997f2d7c1db330331fb696ce6cbd87fa
Chad Drake <chaddrake1313@yahoo.com>
2020-10-16 13:38:09 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://hidden-falls-68641.herokuapp.com',
+  API_ENDPOINT: 'https://hidden-falls-68641.herokuapp.com/api',
   TOKEN_KEY: 'e47b9dfc-08c9-11eb-adc1-0242ac120002',
 };
